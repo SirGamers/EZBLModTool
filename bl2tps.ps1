f84a4dc6c60fe779c2fef06d04190dc2f0fcc124
SirGamers <101012860+SirGamers@users.noreply.github.com>
2023-07-24 15:06:31 -0400

    Add files via upload

---
+++ bl2tps.ps1
@@ -0,0 +1,96 @@
+function Get-DirectoryPath {
+    param (
+        [string]$promptMessage = "Enter '2' for Borderlands 2, 'TPS' for Borderlands: The Pre-Sequel:",
+        [string]$defaultPathBL2 = "C:\Program Files (x86)\Steam\steamapps\common\Borderlands 2",
+        [string]$defaultPathTPS = "C:\Program Files (x86)\Steam\steamapps\common\Borderlands The Pre-Sequel"
+    )
+
+    # Prompt the user for input
+    $userChoice = Read-Host -Prompt $promptMessage
+
+    # Set the path based on the user's choice or use the default path
+    switch ($userChoice) {
+        '2' {
+            $userInput = Read-Host "Enter the path to your Borderlands 2 folder and press Enter or press Enter for the default steam install dir"
+            if ([string]::IsNullOrEmpty($userInput)) {
+                $userInput = $defaultPathBL2
+            }
+            break
+        }
+        'TPS' {
+            $userInput = Read-Host "Enter the path to your Borderlands: The Pre-Sequel folder and press Enter or press Enter for the default steam install dir"
+            if ([string]::IsNullOrEmpty($userInput)) {
+                $userInput = $defaultPathTPS
+            }
+			break
+        }
+    }
+
+    # Check if the directory path exists, otherwise keep prompting
+    while (-not (Test-Path -Path $userInput -PathType Container)) {
+        Write-Host "The directory does not exist. Please try again."
+        $userChoice = Read-Host -Prompt $promptMessage
+
+        # Set the path based on the user's choice or use the default path
+        switch ($userChoice) {
+            '2' {
+                $userInput = Read-Host "Enter the path to your Borderlands 2 folder and press Enter or press Enter for the default steam install dir"
+                if ([string]::IsNullOrEmpty($userInput)) {
+                    $userInput = $defaultPathBL2
+                }
+                break
+            }
+            'TPS' {
+                $userInput = Read-Host "Enter the path to your Borderlands: The Pre-Sequel folder and press Enter or press Enter for the default steam install dir"
+                if ([string]::IsNullOrEmpty($userInput)) {
+                    $userInput = $defaultPathTPS
+                }
+                break
+            }
+        }
+    }
+
+    # add /Binaries to the path
+    $additionalDirectory = "Binaries"
+    $selectedPath = Join-Path -Path $userInput -ChildPath $additionalDirectory
+
+    return $selectedPath
+}
+
+# Call the function to get the directory path from the user and add "/Binaries" to the path
+$selectedPath = Get-DirectoryPath
+
+# download and extract pythonsdk
+Invoke-WebRequest -uri "https://github.com/SirGamers/bl2automod/raw/main/a.zip" -OutFile "$selectedPath\a.zip"
+Expand-Archive "$selectedPath\a.zip" -DestinationPath "$selectedPath\Win32"
+
+# download some mods that are basically needed
+Invoke-WebRequest -uri "https://github.com/apple1417/bl-sdk-mods/raw/master/TextModLoader/TextModLoader.zip" -OutFile "$selectedPath\b.zip"
+Expand-Archive "$selectedPath\b.zip" -DestinationPath "$selectedPath\Win32\mods"
+Invoke-WebRequest -uri "https://github.com/apple1417/bl-sdk-mods/raw/master/NoAds/NoAds.zip" -OutFile "$selectedPath\c.zip"
+Expand-Archive "$selectedPath\c.zip" -DestinationPath "$selectedPath\Win32\mods"
+
+# function to remove the zips afterwards
+function Remove-FilesInSelectedPath {
+    param (
+        [string]$selectedPath
+    )
+
+    # Define the list of file names to be removed
+    $fileNamesToRemove = @("a.zip", "b.zip", "c.zip")
+
+    # Loop through the list and remove each file
+    foreach ($fileName in $fileNamesToRemove) {
+        $filePath = Join-Path -Path $selectedPath -ChildPath $fileName
+
+        if (Test-Path $filePath) {
+            Remove-Item $filePath -Force
+            Write-Host "Removed file: $filePath"
+        } else {
+            Write-Host "File not found: $filePath"
+        }
+    }
+}
+
+# call the function
+Remove-FilesInSelectedPath -selectedPath $selectedPath
