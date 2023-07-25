2cb5860fa5cd7e130418afab6989b470d614149f
SirGamers <101012860+SirGamers@users.noreply.github.com>
2023-07-25 16:09:46 -0400

    Update bl3.ps1

--- bl3.ps1
+++ bl3.ps1
@@ -1,8 +1,38 @@
+function Get-GameSelection {
+    param (
+        [string]$promptMessage = "Please select a game: Borderlands 3 (B) or Tiny Tina's Wonderlands (T)",
+        [string]$defaultGame = "B"
+    )
+
+    # Prompt the user for input
+    $userInput = Read-Host -Prompt $promptMessage
+
+    # If no input provided, use the default game
+    if ([string]::IsNullOrEmpty($userInput)) {
+        $userInput = $defaultGame
+    }
+
+    # Check if the user selected "Borderlands 3" or "Tiny Tina's Wonderlands"
+    while ($userInput -notin @('B', 'T')) {
+        Write-Host "Invalid selection. Please select 'B' for Borderlands 3 or 'T' for Tiny Tina's Wonderlands."
+        $userInput = Read-Host -Prompt $promptMessage
+
+        if ([string]::IsNullOrEmpty($userInput)) {
+            $userInput = $defaultGame
+        }
+    }
+
+    # Return the selected game as a string (either "Borderlands 3" or "Tiny Tina's Wonderlands")
+    return $userInput
+}
+
 function Get-DirectoryPath {
     param (
-        [string]$promptMessage = "Press enter if your Borderlands 3 folder is stored at C:\Program Files (x86)\Steam\steamapps\common\Borderlands 3, otherwise type in the path to BL3 and press enter",
-        [string]$defaultPath = "C:\Program Files (x86)\Steam\steamapps\common\Borderlands 3"
+        [string]$gameSelection,
+        [string]$defaultPath
     )
+
+    $promptMessage = "Press enter if your $($gameSelection) folder is stored at $defaultPath, otherwise type in the path to $($gameSelection) and press enter."
 
     # Prompt the user for input
     $userInput = Read-Host -Prompt $promptMessage
@@ -22,27 +52,9 @@ function Get-DirectoryPath {
         }
     }
 
-    # Append \OakGame\Binaries to the path
-    $additionalDirectory = "OakGame\Binaries"
-    $selectedPath = Join-Path -Path $userInput -ChildPath $additionalDirectory
-
-    return $selectedPath
+    return $userInput
 }
 
-# Call the function to get the selected path
-$selectedPath = Get-DirectoryPath
-
-# Define the file URLs
-$bl3Dx11InjectionUrl = "https://github.com/FromDarkHell/BL3DX11Injection/releases/download/v1.1.3/D3D11.zip"
-$openHotfixLoaderUrl = "https://github.com/apple1417/OpenHotfixLoader/releases/download/v1.6/OpenHotfixLoader.zip"
-
-# Define the file names to be removed
-$fileNamesToRemove = @("a.zip", "b.zip")
-
-# Define the file name to be removed from a different directory
-$fileNameToRemove = "LICENSE"
-
-# Function to download and extract files
 function Download-AndExtractFile {
     param (
         [string]$url,
@@ -64,19 +76,13 @@ function Download-AndExtractFile {
     }
 }
 
-# Download and extract bl3dx11injection
-Download-AndExtractFile -url $bl3Dx11InjectionUrl -destinationPath $selectedPath\Win64
-
-# Download and extract openhotfixloader
-Download-AndExtractFile -url $openHotfixLoaderUrl -destinationPath (Join-Path -Path $selectedPath\Win64 -ChildPath "Plugins")
-
-# Function to remove files in the selected path
 function Remove-FilesInSelectedPath {
     param (
-        [string]$selectedPath
+        [string]$selectedPath,
+        [string[]]$fileNames
     )
 
-    foreach ($fileName in $fileNamesToRemove) {
+    foreach ($fileName in $fileNames) {
         $filePath = Join-Path -Path $selectedPath -ChildPath $fileName
 
         if (Test-Path $filePath) {
@@ -88,24 +94,33 @@ function Remove-FilesInSelectedPath {
     }
 }
 
-# Function to remove a file from a different directory within selected path
-function Remove-FileFromDifferentDirectory {
-    param (
-        [string]$selectedPath
-    )
+# Call the function to get the game selection
+$selectedGame = Get-GameSelection
 
-    $filePath = Join-Path -Path (Join-Path -Path $selectedPath -ChildPath "Win64\Plugins") -ChildPath $fileNameToRemove
-
-    if (Test-Path $filePath) {
-        Remove-Item $filePath -Force
-        Write-Host "Removed file: $filePath"
-    } else {
-        Write-Host "File not found: $filePath"
-    }
+# Set the default path based on the selected game
+$defaultPath = if ($selectedGame -eq 'T') {
+    "C:\Program Files (x86)\Steam\steamapps\common\Tiny Tina's Wonderlands"
+} else {
+    "C:\Program Files (x86)\Steam\steamapps\common\Borderlands 3"
 }
 
+# Call the function to get the selected path based on the game selection and default path
+$selectedPath = Get-DirectoryPath -gameSelection $selectedGame -defaultPath $defaultPath
+
+# Define the file URLs and names to be removed
+$bl3Dx11InjectionUrl = "https://github.com/FromDarkHell/BL3DX11Injection/releases/download/v1.1.3/D3D11.zip"
+$openHotfixLoaderUrl = "https://github.com/apple1417/OpenHotfixLoader/releases/download/v1.6/OpenHotfixLoader.zip"
+$fileNamesToRemove = @("a.zip", "b.zip")
+$fileNameToRemove = "LICENSE"
+
+# Download and extract bl3dx11injection
+Download-AndExtractFile -url $bl3Dx11InjectionUrl -destinationPath $selectedPath\Win64
+
+# Download and extract openhotfixloader
+Download-AndExtractFile -url $openHotfixLoaderUrl -destinationPath (Join-Path -Path $selectedPath\Win64 -ChildPath "Plugins")
+
 # Call the function to remove files
-Remove-FilesInSelectedPath -selectedPath $selectedPath
+Remove-FilesInSelectedPath -selectedPath $selectedPath -fileNames $fileNamesToRemove
 
 # Call the function to remove a file from a different directory
-Remove-FileFromDifferentDirectory -selectedPath $selectedPath
+Remove-FilesInSelectedPath -selectedPath (Join-Path -Path $selectedPath\Win64 -ChildPath "Plugins") -fileNames $fileNameToRemove
